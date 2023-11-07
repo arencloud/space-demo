905f7529815eaa30fbb3b57975da18eed1f85381
arencloud <info@arencloud.com>
2023-11-07 14:56:39 +0000

    add simple demo code

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello from OpenShift Dev Spaces!!!")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, header := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, header)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Welcome to OpenShift DevSpaces!!!")
+	
+	http.HandleFunc("hello", hello)
+	http.HandleFunc("headers", headers)
+	http.ListenAndServe(":8080", nil)
 }
