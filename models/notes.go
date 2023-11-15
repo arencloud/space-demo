79676dea98e2af6f001c7523abebd1fe5ea4d6cb
arencloud <info@arencloud.com>
2023-11-15 12:06:02 +0100

    fixed devfile

--- models/notes.go
+++ models/notes.go
@@ -1,33 +1,32 @@
 package models
 
 import (
-	"time"
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"time"
 )
 
-
 type CreateNoteSchema struct {
-	Title string `json:"title" validate:"required"`
-	Content string `json:"content" validate:"required"`
-	Category string `json:"category,omitempty"`
-	Published bool `json:"published,omitempty"`
+	Title     string `json:"title" validate:"required"`
+	Content   string `json:"content" validate:"required"`
+	Category  string `json:"category,omitempty"`
+	Published bool   `json:"published,omitempty"`
 }
 
 type UpdateNoteSchema struct {
-	Title string `json:"title,omitempty"`
-	Content string `json:"content,omitempty"`
-	Category string `json:"category,omitempty"`
-	Published *bool `json:"published,omitempty"`
+	Title     string `json:"title,omitempty"`
+	Content   string `json:"content,omitempty"`
+	Category  string `json:"category,omitempty"`
+	Published *bool  `json:"published,omitempty"`
 }
 
 type Note struct {
-	ID string `gorm:"type:char(36);primary_key" json:"id,omitempty"`
-	Title string `gorm:"type:varchar(255);uniqueIndex:idx_notes_title,LENGTH(255);not null" json:"title,omitempty"`
-	Content string `gorm:"not null" json:"content,omitempty"`
-	Category string `gorm:"varchar(100)" json:"category,omitempty"`
-	Published bool `gorm:"default:false;not null" json:"published"`
+	ID        string    `gorm:"type:char(36);primary_key" json:"id,omitempty"`
+	Title     string    `gorm:"type:varchar(255);uniqueIndex:idx_notes_title,LENGTH(255);not null" json:"title,omitempty"`
+	Content   string    `gorm:"not null" json:"content,omitempty"`
+	Category  string    `gorm:"varchar(100)" json:"category,omitempty"`
+	Published bool      `gorm:"default:false;not null" json:"published"`
 	CreatedAt time.Time `gorm:"not null;default:'1970-01-01 00:00:01'" json:"createdAt,omitempty"`
 	UpdatedAt time.Time `gorm:"not null;default:'1970-01-01 00:00:01';ON UPDATE CURRENT_TIMESTAMP" json:"updatedAt,omitempty"`
 }
@@ -36,7 +35,6 @@ func (n *Note) PreCreate(tx *gorm.DB) (err error) {
 	n.ID = uuid.New().String()
 	return nil
 }
-
 
 var validate = validator.New()
 
