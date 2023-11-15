01031700fff2ca40df6292ac18cb18d96583375d
arencloud <info@arencloud.com>
2023-11-15 10:20:10 +0100

    fixed devfile

--- initlib/config.go
+++ initlib/config.go
@@ -3,22 +3,21 @@ package initlib
 import "os"
 
 type Config struct {
-	DBHost     string
-	DBUserName string
-	DBUserPwd  string
-	DBName     string
-	DBPort     string
+	DBHost       string
+	DBUserName   string
+	DBUserPwd    string
+	DBName       string
+	DBPort       string
 	ClientOrigin string
 }
 
-
 func LoadConfig() (config Config) {
 	return Config{
-		DBHost: os.Getenv("MYSQL_HOST"),
-		DBUserName: os.Getenv("MYSQL_USER"),
-		DBUserPwd: os.Getenv("MYSQL_PASSWORD"),
-		DBName: os.Getenv("MYSQL_DATABASE"),
-		DBPort: os.Getenv("MYSQL_PORT"),
+		DBHost:       os.Getenv("SPACE_DEMO_DB_HOST"),
+		DBUserName:   os.Getenv("SPACE_DEMO_DB_USER"),
+		DBUserPwd:    os.Getenv("SPACE_DEMO_DB_PASSWORD"),
+		DBName:       os.Getenv("SPACE_DEMO_DB_DATABASE"),
+		DBPort:       os.Getenv("SPACE_DEMO_DB_PORT"),
 		ClientOrigin: os.Getenv("CLIENT_ORIGIN"),
 	}
 }
